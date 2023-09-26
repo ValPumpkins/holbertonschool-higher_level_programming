15e932153b5fa54ee4b2a31f56b51f2fc0c529d6
ValentinePumpkins <quignon.val@gmail.com>
2023-09-26 16:17:15 +0200

    rm all the opérator loop + change order

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
-import sys
-from calculator_1 import add, sub, mul, div
-
 if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
 
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
@@ -13,19 +12,15 @@ if __name__ == "__main__":
     operator = sys.argv[2]
     b = int(sys.argv[3])
 
-    if operator == "+":
-        result = add(a, b)
-    elif operator == "-":
-        result = sub(a, b)
-    elif operator == "*":
-        result = mul(a, b)
-    elif operator == "/":
-        if b == 0:
-            print("Error: Division by zero")
-            sys.exit(1)
-        result = div(a, b)
-    else:
+    calcul = {
+        "+": add,
+        "-": sub,
+        "*": mul,
+        "/": div
+        }
+
+    if operator not in calcul:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    print("{} {} {} = {}".format(a, operator, b, result))
+    print("{} {} {} = {}".format(a, operator, b, calcul[operator](a, b)))
