8e9f822f801e4463ce54ab46361b872d66496178
ValentinePumpkins <quignon.val@gmail.com>
2023-10-02 16:10:25 +0200

    T06: change space

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         i = 0
         for num in row:
-            print("{:d}".format(num), end=" ")
+            print("{:d}".format(num), end="")
             if i < len(row) - 1:
                 print(" ", end="")
             i += 1
