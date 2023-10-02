416f3a7fd5e20966e6f3b9c3680282bb22f2cbb2
ValentinePumpkins <quignon.val@gmail.com>
2023-10-02 16:09:23 +0200

    T06: add cond + incremate

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,10 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i, num in enumerate(row):
-            print("{:d} ".format(num), end="")
+        i = 0
+        for num in row:
+            print("{:d}".format(num), end=" ")
+            if i < len(row) - 1:
+                print(" ", end="")
+            i += 1
         print()
