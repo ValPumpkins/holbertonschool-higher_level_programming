a0a5ae0d1bf97a0a5e0a709272b0a33eff072011
ValentinePumpkins <quignon.val@gmail.com>
2023-10-04 14:44:38 +0200

    T13:  weighted avg of all int tuples

---
+++ python-more_data_structures/100-weight_average.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    if my_list == [] or my_list == None:
+        return 0
+    totalScore = 0
+    totalWeight = 0
+
+    for score, weight in my_list:
+        totalScore += score * weight
+        totalWeight += weight
+
+    if totalWeight == 0:
+        return 0
+
+    return totalScore / totalWeight
