275c2ec85c7a5d33626d01b42484a4ee3942bda7
ValentinePumpkins <quignon.val@gmail.com>
2023-10-09 18:05:52 +0200

    T00 - edit txt files + change return in func

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -13,9 +13,13 @@ def add_integer(a, b=98):
     Raises:
         TypeError: if a or b is not an integer or float
     """
-    if ((not isinstance(a, int) and not isinstance(a, float))):
+    
+    if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
-    if ((not isinstance(b, int) and not isinstance(b, float))):
+    if type(b) != int and type(b) != float:
         raise TypeError("b must be an integer")
 
-    return (int(a) + int(b))
+    a = int(a)
+    b = int(b)
+
+    return a + b
