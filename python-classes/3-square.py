9773bd06c362b36fe675fd243e707875990c4418
ValentinePumpkins <quignon.val@gmail.com>
2023-10-09 12:25:03 +0200

    T03 - return square area

---
+++ python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+""" Class square that defines a square """
+
+
+class Square:
+    """ Class Square that defines a square w/ size """
+
+    def __init__(self, size=0):
+        """
+        Initialize new square w/ optionnal size
+        Args:
+            size (int, optionnal): size of square. Default is 0
+        Raises:
+            TypeError: if size is not an int
+            ValueError: if size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """ Return current area of square """
+        return self.__size ** 2
