9031cf449b63d3cdd16d0a94515e677ed45212bc
ValentinePumpkins <quignon.val@gmail.com>
2023-10-09 11:11:45 +0200

    T01 - def square size (private inst)

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+""" Class Square that defines a square """
+
+
+class Square:
+    """ Class Square that defines a square w/ size """
+
+    def __init__(self, size):
+        """ Size of square is a private instance attribute """
+        self.__size = size
