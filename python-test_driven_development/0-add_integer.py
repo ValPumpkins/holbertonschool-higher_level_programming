bd296eebd9fea5e47a7a02b7e4be0b8d279e06c7
ValentinePumpkins <quignon.val@gmail.com>
2023-10-09 16:51:11 +0200

    T00 - int addition + tests

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+""" Func thats add 2 integers """
+
+
+def add_integer(a, b=98):
+    """
+    Args:
+        a (int or float): first integer
+        b (int or float): second integer
+    Returns:
+        int: sum of a and b
+    Raises:
+        TypeError: if a or b is not an integer or float
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
