e043043dea045e6073d924643daac03eb91c7908
ValentinePumpkins <quignon.val@gmail.com>
2023-10-16 17:13:54 +0200

    T06: not implemented

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Defines class BaseGeometry """
+
+
+class BaseGeometry:
+    """ Represent class BaseGeometry """
+    def area(self):
+        """ Not implemented """
+        raise Exception("area() is not implemented")
