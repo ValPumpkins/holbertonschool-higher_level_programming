a4e965dd286c1896d8934e3d01c09fd77a727b74
ValentinePumpkins <quignon.val@gmail.com>
2023-10-16 17:46:40 +0200

    T07: validate a parameter as an int

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Defines class BaseGeometry """
+
+
+class BaseGeometry:
+    """ Represent class BaseGeometry """
+    def area(self):
+        """ Not implemented """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ Validate a parameter as an int
+        Args:
+            name (str): name of the parameter
+            value (int): parameter to validate
+        Raises:
+            TypeError: if value is not an int
+            ValueError: if value is <= 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
