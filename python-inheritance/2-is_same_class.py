79107dd7bffcc97714510bb0ebcac61d913a346d
ValentinePumpkins <quignon.val@gmail.com>
2023-10-16 16:34:55 +0200

    T02:  instanec of class ?

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Function that returns is instance if part of the class"""
+
+def is_same_class(obj, a_class):
+    """ Function that returns is instance if part of the class
+    Args:
+        obj: object
+        a_class: class type
+    Returns:
+        True: if object is instance of a_class
+        False: if not
+     """
+    return type(obj) is a_class
