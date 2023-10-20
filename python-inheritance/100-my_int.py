cd16cee8dd4d8881876b670b8cb9ad850777bfe8
ValentinePumpkins <quignon.val@gmail.com>
2023-10-20 10:51:03 +0200

    add docstring

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+""" MyInt class """
 
 class MyInt(int):
+    """ invert True & False
+    Args:
+        number (int): integer
+    """
     def __eq__(self, number):
         return super().__ne__(number)
 
