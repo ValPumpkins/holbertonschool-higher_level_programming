f5341c51cfc9f767acaf371c99557c60e814cac7
ValentinePumpkins <quignon.val@gmail.com>
2023-10-20 10:53:12 +0200

    correct pycodestyle

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ MyInt class """
 
+
 class MyInt(int):
     """ invert True & False
     Args:
-        number (int): integer
+        int (int): mother class
     """
     def __eq__(self, number):
         return super().__ne__(number)
