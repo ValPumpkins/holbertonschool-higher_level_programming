15a364da6c3f90ce5fb6bb4e66ce299dc9ddc176
ValentinePumpkins <quignon.val@gmail.com>
2023-10-24 13:37:45 +0200

    static method json to string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Defines a base model class """
 
+import json
 
 class Base:
     """ Represent the base model for this project
@@ -19,3 +20,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ Return the JSON string rep of a list dict """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
