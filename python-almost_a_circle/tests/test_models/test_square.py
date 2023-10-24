4a6370f7d6abdd1f60da3d6d3b1e482d2a336a80
ValentinePumpkins <quignon.val@gmail.com>
2023-10-24 09:14:02 +0200

    add test update for Square

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -14,10 +14,15 @@ class TestSquare(TestRectangle):
     # INHERITANCE
     def test_square_inherits_from_rectangle(self):
         """ Testing inheritance """
-        self.assertTrue(issubclass(Square, Rectangle),
+        square = Square(5)
+        self.assertTrue(isinstance(square, Rectangle),
                         "Square should inherit from Rectangle")
 
     # INIT
+    def test_square_init(self):
+        square = Square(5)
+        self.assertEqual(square.size, 5)
+
     def test_square(self):
         """ Testing square initialization """
         square = Square(5, 2, 3, 1)
@@ -44,45 +49,75 @@ class TestSquare(TestRectangle):
         square = Square(5, 2, 3, 1)
         square.size = 10
         self.assertEqual(square.size, 10)
-        self.assertEqual(square.width, 10)
-        self.assertEqual(square.height, 10)
 
     def test_size_setter_negative(self):
         """ Testing size setter with negative value """
         with self.assertRaises(ValueError):
-            square = Square(5, 2, 3, 1)
+            square = Square(5)
             square.size = -10
-            self.assertEqual(square.size, -10)
-            self.assertEqual(square.width, -10)
-            self.assertEqual(square.height, -10)
 
     def test_size_setter_zero(self):
         """ Testing size setter with zero value """
         with self.assertRaises(ValueError):
             square = Square(5, 2, 3, 1)
             square.size = 0
-            self.assertEqual(square.size, 0)
-            self.assertEqual(square.width, 0)
-            self.assertEqual(square.height, 0)
 
     def test_size_setter_float(self):
         """ Testing size setter with float value """
         with self.assertRaises(TypeError):
             square = Square(5, 2, 3, 1)
             square.size = 3.14
-            self.assertEqual(square.size, 3.14)
-            self.assertEqual(square.width, 3.14)
-            self.assertEqual(square.height, 3.14)
 
     def test_size_setter_string(self):
         """ Testing size setter with string value """
         with self.assertRaises(TypeError):
             square = Square(5, 2, 3, 1)
             square.size = "hello"
-            self.assertEqual(square.size, "hello")
-            self.assertEqual(square.width, "hello")
-            self.assertEqual(square.height, "hello")
 
+    # UPDATE
+    def test_update_args(self):
+        """ Testing square initialization with args """
+        square = Square(3, 1, 2, 42)
+        square.update(1, 4, 3, 0, 0)
+        self.assertEqual(square.id, 1)
+        self.assertEqual(square.size, 4)
+        self.assertEqual(square.x, 3)
+        self.assertEqual(square.y, 0)
+
+    def test_update_kwargs_only(self):
+        """ Testing square initialization with kwargs """
+        square = Square(3, 1, 2, 42)
+        square.update(size=4, x=3, y=0, id=1)
+        self.assertEqual(square.id, 1)
+        self.assertEqual(square.size, 4)
+        self.assertEqual(square.x, 3)
+        self.assertEqual(square.y, 0)
+
+    def test_update_kwargs_args1(self):
+        """ Testing square initialization with kwargs """
+        square = Square(3, 1, 2, 42)
+        square.update(1, 4, size=3)
+        self.assertEqual(square.id, 1)
+        self.assertEqual(square.size, 4)
+        self.assertEqual(square.x, 1)
+        self.assertEqual(square.y, 2)
+
+    def test_update_args_kwargs2(self):
+        square = Square(3, 1, 2, 42)
+        square.update(1, size=4, x=0)
+        self.assertEqual(square.id, 1)
+        self.assertEqual(square.size, 3)
+        self.assertEqual(square.x, 1)
+        self.assertEqual(square.y, 2)
+
+    def test_square_update_unknown_attributes(self):
+        """ Testing update with unknown attributes """
+        square = Square(3, 2, 4, 5)
+        square.update(1, size=4, x=0)
+        self.assertEqual(square.id, 1)
+        self.assertEqual(square.size, 3)
+        self.assertEqual(square.x, 2)
+        self.assertEqual(square.y, 4)
 
 
 if __name__ == "__main__":
