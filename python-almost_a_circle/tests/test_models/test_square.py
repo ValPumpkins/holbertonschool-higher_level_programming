d5eb53bdc556d98d433da36263f6825d5619f2f4
ValentinePumpkins <quignon.val@gmail.com>
2023-10-24 08:24:30 +0200

    add test for Square (size)

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -33,6 +33,57 @@ class TestSquare(TestRectangle):
         square = Square(3, 1, 2, 42)
         self.assertEqual(str(square), "[Square] (42) 1/2 - 3")
 
+    # SIZE VALIDATION
+    def test_size_getter(self):
+        """ Testing size getter """
+        square = Square(5, 2, 3, 1)
+        self.assertEqual(square.size, 5)
+
+    def test_size_setter(self):
+        """ Testing size setter """
+        square = Square(5, 2, 3, 1)
+        square.size = 10
+        self.assertEqual(square.size, 10)
+        self.assertEqual(square.width, 10)
+        self.assertEqual(square.height, 10)
+
+    def test_size_setter_negative(self):
+        """ Testing size setter with negative value """
+        with self.assertRaises(ValueError):
+            square = Square(5, 2, 3, 1)
+            square.size = -10
+            self.assertEqual(square.size, -10)
+            self.assertEqual(square.width, -10)
+            self.assertEqual(square.height, -10)
+
+    def test_size_setter_zero(self):
+        """ Testing size setter with zero value """
+        with self.assertRaises(ValueError):
+            square = Square(5, 2, 3, 1)
+            square.size = 0
+            self.assertEqual(square.size, 0)
+            self.assertEqual(square.width, 0)
+            self.assertEqual(square.height, 0)
+
+    def test_size_setter_float(self):
+        """ Testing size setter with float value """
+        with self.assertRaises(TypeError):
+            square = Square(5, 2, 3, 1)
+            square.size = 3.14
+            self.assertEqual(square.size, 3.14)
+            self.assertEqual(square.width, 3.14)
+            self.assertEqual(square.height, 3.14)
+
+    def test_size_setter_string(self):
+        """ Testing size setter with string value """
+        with self.assertRaises(TypeError):
+            square = Square(5, 2, 3, 1)
+            square.size = "hello"
+            self.assertEqual(square.size, "hello")
+            self.assertEqual(square.width, "hello")
+            self.assertEqual(square.height, "hello")
+
+
 
 if __name__ == "__main__":
     unittest.main()
