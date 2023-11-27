361b18e0e3871f1f11533dd9458082ffb84ef67f
ValentinePumpkins <quignon.val@gmail.com>
2023-11-27 15:26:00 +0100

    correct pycodestyle

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -11,11 +11,12 @@ if __name__ == "__main__":
     state_name = sys.argv[4]
 
     connection = MySQLdb.connect(host="localhost",
-                                port=3306,
-                                user=mysql_username,
-                                passwd=mysql_password,
-                                db=database_name
-                                )
+                                 port=3306,
+                                 user=mysql_username,
+                                 passwd=mysql_password,
+                                 db=database_name
+                                 )
+    
     cursor = connection.cursor()
 
     query = "SELECT cities.name FROM cities \
