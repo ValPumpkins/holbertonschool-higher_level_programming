16c6ec0033ead6c2f407e9e376b9b4927a2f1944
ValentinePumpkins <quignon.val@gmail.com>
2023-11-27 14:45:45 +0100

    rm arg validation + add .format + binary to query

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -5,11 +5,6 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: {} <username> <password> <database> <state_name>"
-              .format(sys.argv[0]))
-        sys.exit(1)
-
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
@@ -22,8 +17,9 @@ if __name__ == "__main__":
                                  db=database_name
                                  )
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC",
-                   (state_name,))
+    cursor.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(state_name)
+        )
 
     states = cursor.fetchall()
 
